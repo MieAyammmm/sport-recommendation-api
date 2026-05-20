2eebcb095eadfa127978fad79c24d4eb1e5c6203
dani <dhannizd2@gmail.com>
2026-05-20 09:07:50 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DecisionTreeController;
 
+Route::get('/decision-tree', [DecisionTreeController::class, 'index']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
